c4c6ce77a8b6dd8666ac8e6d072f2bb55a9f2c93
byariel95 <ariel@dooply.net>
2020-02-27 09:48:42 -0600

    conection with database

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-
+import {createConnection} from 'typeorm';
  
 const app = express();
-
+createConnection();
 
 //middlewares 
 app.use(cors()); 
@@ -12,6 +12,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 
-app.listen(3000);
-console.log('server on port: ',3000);
+async function runserver (){
+     await app.listen(3000);
+    console.log('server on port: ',3000);
+};
+
+runserver();
+
  
